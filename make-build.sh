b07642da9778284fb042397ae53c3e77f191a0dd
Gintama4141 <106681440+Gintama4141@users.noreply.github.com>
2025-04-25 21:13:38 +0700

    Update make-build.sh

--- make-build.sh
+++ make-build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+. ./scripts/INCLUDE.sh
+
+# Exit on error
+set -e
+
 # Profile info
 make info
 
@@ -7,114 +12,141 @@ make info
 PROFILE=""
 PACKAGES=""
 
+# Base
+PACKAGES+=" dnsmasq-full cgi-io libiwinfo libiwinfo-data libiwinfo-lua liblua \
+liblucihttp liblucihttp-lua libubus-lua lua libc block-mount adb curl \
+coreutils-stty coreutils-base64 coreutils-stat coreutils-sleep htop screen \
+rpcd rpcd-mod-file rpcd-mod-iwinfo rpcd-mod-luci rpcd-mod-rrdns jq jshn \
+libjson-script liblucihttp liblucihttp-lua lolcat tar unzip python3-pip \
+zram-swap uhttpd uhttpd-mod-ubus uuidgen wget-ssl zoneinfo-asia zoneinfo-core \
+luci luci-base luci-ssl luci-compat luci-lib-base luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio \
+luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system luci-proto-ipv6 luci-proto-ppp"
+
 # Modem and UsbLAN Driver
 PACKAGES+=" kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 kmod-usb-net-asix kmod-usb-net-asix-ax88179"
-PACKAGES+=" kmod-mii kmod-usb-net kmod-usb-wdm kmod-usb-net-qmi-wwan uqmi luci-proto-qmi \
-kmod-usb-net-cdc-ether kmod-usb-serial-option kmod-usb-serial kmod-usb-serial-wwan qmi-utils \
-kmod-usb-serial-qualcomm kmod-usb-acm kmod-usb-net-cdc-ncm kmod-usb-net-cdc-mbim umbim \
-modemmanager luci-proto-modemmanager libmbim libqmi usbutils luci-proto-mbim luci-proto-ncm \
-kmod-usb-net-huawei-cdc-ncm kmod-usb-net-cdc-ether kmod-usb-net-rndis kmod-usb-net-sierrawireless kmod-usb-ohci kmod-usb-serial-sierrawireless \
-kmod-usb-uhci kmod-usb2 kmod-usb-ehci kmod-usb-net-ipheth usbmuxd libusbmuxd-utils libimobiledevice-utils usb-modeswitch kmod-nls-utf8 mbim-utils xmm-modem \
-kmod-phy-broadcom kmod-phylib-broadcom kmod-tg3"
+PACKAGES+=" kmod-mii comgt-ncm kmod-usb-net kmod-usb-net-cdc-ncm kmod-usb-net-huawei-cdc-ncm kmod-usb-wdm \
+kmod-usb-net-cdc-ether usb-modeswitch kmod-usb-net-rndis kmod-usb-net-sierrawireless kmod-usb-serial-sierrawireless \
+kmod-usb-uhci kmod-usb-net-qmi-wwan uqmi luci-proto-qmi kmod-usb-ohci kmod-usb-net-cdc-mbim umbim \
+kmod-usb2 kmod-usb-ehci kmod-usb-serial-option kmod-usb-serial-qualcomm kmod-usb-serial \
+kmod-usb-serial-wwan libqmi qmi-utils libmbim mbim-utils luci-proto-mbim \
+modemmanager luci-proto-modemmanager usbutils xmm-modem \
+kmod-usb3 kmod-macvlan kmod-nls-utf8 kmod-usb-storage kmod-usb-storage-uas"
 
 # Modem Tools
-PACKAGES+=" modeminfo luci-app-modeminfo atinout modemband luci-app-modemband luci-app-mmconfig sms-tool luci-app-sms-tool-js luci-app-lite-watchdog luci-app-3ginfo-lite picocom minicom"
+PACKAGES+=" modeminfo luci-app-modeminfo atinout modemband luci-app-modemband luci-app-mmconfig sms-tool luci-app-sms-tool-js luci-app-3ginfo-lite picocom minicom"
+
+# Modem Info
+PACKAGES+=" modeminfo-serial-tw modeminfo-serial-dell modeminfo-serial-xmm modeminfo-serial-fibocom modeminfo-serial-sierra"
 
 # Tunnel option
-OPENCLASH_FW3="coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun unzip luci-compat luci luci-base luci-app-openclash"
-OPENCLASH_FW4="coreutils-nohup bash dnsmasq-full curl ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base luci-app-openclash"
-NEKO="bash kmod-tun php8 php8-cgi luci-app-neko"
-PASSWALL="ipset ipt2socks iptables iptables-legacy iptables-mod-iprange iptables-mod-socket iptables-mod-tproxy kmod-ipt-nat coreutils coreutils-base64 coreutils-nohup curl dns2socks ip-full libuci-lua lua luci-compat luci-lib-jsonc microsocks resolveip tcping unzip dns2tcp brook hysteria trojan-go xray-core xray-plugin sing-box chinadns-ng haproxy ip6tables-mod-nat kcptun-client naiveproxy pdnsd-alt shadowsocks-libev-ss-local shadowsocks-libev-ss-redir shadowsocks-libev-ss-server shadowsocks-rust-sslocal shadowsocksr-libev-ssr-local shadowsocksr-libev-ssr-redir shadowsocksr-libev-ssr-server simple-obfs trojan-plus v2ray-core v2ray-plugin luci-app-passwall luci-app-passwall2"
-if [ "$2" == "openclash" ]; then
-    PACKAGES+=" $([ "$(echo "$BRANCH" | cut -d'.' -f1)" == "21" ] && echo "$OPENCLASH_FW3" || echo "$OPENCLASH_FW4")"
-elif [ "$2" == "neko" ]; then
-    PACKAGES+=" $NEKO"
-elif [ "$2" == "passwall" ]; then
-    PACKAGES+=" $PASSWALL"
-elif [ "$2" == "neko-openclash" ]; then
-    PACKAGES+=" $([ "$(echo "$BRANCH" | cut -d'.' -f1)" == "21" ] && echo "$OPENCLASH_FW3" || echo "$OPENCLASH_FW4") $NEKO"
-elif [ "$2" == "openclash-passwall" ]; then
-    PACKAGES+=" $([ "$(echo "$BRANCH" | cut -d'.' -f1)" == "21" ] && echo "$OPENCLASH_FW3" || echo "$OPENCLASH_FW4") $PASSWALL"
-elif [ "$2" == "neko-passwall" ]; then
-    PACKAGES+=" $NEKO $PASSWALL"
-elif [ "$2" == "openclash-passwall-neko" ]; then
-    PACKAGES+=" $([ "$(echo "$BRANCH" | cut -d'.' -f1)" == "21" ] && echo "$OPENCLASH_FW3" || echo "$OPENCLASH_FW4") $NEKO $PASSWALL"
-fi
+OPENCLASH+="coreutils-nohup bash ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag kmod-nft-tproxy luci-app-openclash"
+NIKKI+="nikki luci-app-nikki"
+PASSWALL+="chinadns-ng resolveip dns2socks dns2tcp ipt2socks microsocks tcping xray-core xray-plugin luci-app-passwall"
 
-# Adguardhome
-PACKAGES+=" luci-app-adguardhome ca-certificates ca-bundle tar unzip bind-tools"
+# Tunnel options handling
+handle_tunnel_option() {
+    case "$1" in
+        "openclash")
+            PACKAGES+=" $OPENCLASH"
+            ;;
+        "nikki")
+            PACKAGES+=" $NIKKI"
+            ;;
+        "openclash-passwall")
+            PACKAGES+=" $OPENCLASH $PASSWALL"
+            ;;
+        "nikki-openclash")
+            PACKAGES+=" $NIKKI $OPENCLASH"
+            ;;
+        "no-tunnel")
+            PACKAGES+=""
+            ;;
+    esac
+}
 
-# NAS and Hard disk tools
-PACKAGES+=" luci-app-diskman luci-app-hd-idle luci-app-disks-info smartmontools kmod-usb-storage kmod-usb-storage-uas ntfs-3g"
-PACKAGES+=" samba4-server luci-app-samba4 aria2 ariang luci-app-aria2 luci-app-tinyfm"
-
-# Docker
-PACKAGES+=" docker docker-compose dockerd luci-app-dockerman"
+# Nas and storage
+PACKAGES+=" luci-app-diskman luci-app-tinyfilemanager"
 
 # Bandwidth And Network Monitoring
-PACKAGES+=" internet-detector luci-app-internet-detector internet-detector-mod-modem-restart nlbwmon luci-app-nlbwmon vnstat2 vnstati2 luci-app-vnstat2 luci-app-netmonitor"
+PACKAGES+=" internet-detector luci-app-internet-detector vnstat2 vnstati2 luci-app-vnstat2 netdata luci-app-netmonitor"
 
-# Speedtest
-PACKAGES+=" librespeed-go python3-speedtest-cli iperf3 luci-app-netspeedtest"
+# Remote Services
+PACKAGES+=" tailscale luci-app-tailscale"
 
-# Base64 Encode Decode
-PACKAGES+=" luci-app-base64"
+# speedtest and limit bandwidth
+PACKAGES+=" speedtestcli luci-app-eqosplus"
 
-# Argon Theme
-PACKAGES+=" luci-theme-argon luci-app-argon-config"
-
-# Alpha Theme
-PACKAGES+=" luci-theme-alpha luci-app-alpha-config"
-
-# RTA Theme
-PACKAGES+=" luci-theme-rta luci-app-rtaconfig"
+# Theme
+PACKAGES+=" luci-theme-argon luci-theme-alpha"
 
 # PHP8
-PACKAGES+=" libc php8 php8-fastcgi php8-fpm php8-mod-session php8-mod-ctype php8-mod-fileinfo php8-mod-zip php8-mod-iconv php8-mod-mbstring coreutils-stat zoneinfo-asia"
+PACKAGES+=" php8 php8-fastcgi php8-fpm php8-mod-session php8-mod-ctype php8-mod-fileinfo php8-mod-zip php8-mod-iconv php8-mod-mbstring"
 
-# Misc and some custom .ipk files
-misc=""
-if [ "${RELEASE_BRANCH%:*}" == "openwrt" ]; then
-    misc+=" luci-app-temp-status luci-app-cpu-status-mini"
-elif [ "${RELEASE_BRANCH%:*}" == "immortalwrt" ]; then
-    misc+=" "
-fi
+# More
+PACKAGES+=" luci-app-poweroff luci-app-ramfree luci-app-ttyd luci-app-lite-watchdog luci-app-ipinfo luci-app-droidnet luci-app-mactodong"
 
-if [ "$(echo "$BRANCH" | cut -d'.' -f1)" == "21" ]; then
-    misc+=" iptables-mod-ipopt"
-else
-    misc+=" "
-fi
-
-if [ "$1" == "rpi-4" ]; then
-    misc+=" kmod-i2c-bcm2835 i2c-tools kmod-i2c-core kmod-i2c-gpio luci-app-oled"
-elif [ "$ARCH_2" == "x86_64" ]; then
-    misc+=" kmod-iwlwifi iw-full pciutils"
-fi
-
-if [ "$TYPE" == "AMLOGIC" ]; then
-    PACKAGES+=" luci-app-amlogic ath9k-htc-firmware btrfs-progs hostapd hostapd-utils kmod-ath kmod-ath9k kmod-ath9k-common kmod-ath9k-htc kmod-cfg80211 kmod-crypto-acompress kmod-crypto-crc32c kmod-crypto-hash kmod-fs-btrfs kmod-mac80211 wireless-tools wpa-cli wpa-supplicant"
-    EXCLUDED+=" -procd-ujail"
-fi
-
-PACKAGES+=" $misc zram-swap adb parted losetup resize2fs luci luci-ssl block-mount luci-app-poweroff luci-app-log-viewer luci-app-ramfree htop bash curl wget wget-ssl tar unzip unrar gzip jq luci-app-ttyd nano httping screen openssh-sftp-server"
-
-# Exclude package (must use - before packages name)
-EXCLUDED=""
-if [ "${RELEASE_BRANCH%:*}" == "openwrt" ]; then
-    EXCLUDED+=" -dnsmasq"
-elif [ "${RELEASE_BRANCH%:*}" == "immortalwrt" ]; then
-    EXCLUDED+=" -dnsmasq -automount -libustream-openssl -default-settings-chn -luci-i18n-base-zh-cn"
-    if [ "$ARCH_2" == "x86_64" ]; then
-      EXCLUDED+=" -kmod-usb-net-rtl8152-vendor"
+# Handle profile-specific packages
+handle_profile_packages() {
+    if [ "$1" == "rpi-4" ]; then
+        PACKAGES+=" kmod-i2c-bcm2835 i2c-tools kmod-i2c-core kmod-i2c-gpio"
+    elif [ "$ARCH_2" == "x86_64" ]; then
+        PACKAGES+=" kmod-iwlwifi iw-full pciutils"
     fi
+
+    case "${TYPE}" in
+        "OPHUB")
+            PACKAGES+=" btrfs-progs kmod-fs-btrfs luci-app-amlogic"
+            EXCLUDED+=" -procd-ujail"
+            ;;
+        "ULO")
+            PACKAGES+=" luci-app-amlogic"
+            EXCLUDED+=" -procd-ujail"
+            ;;
+    esac
+}
+
+# Handle release branch specific packages
+handle_release_packages() {
+    if [ "${BASE}" == "openwrt" ]; then
+        PACKAGES+=" wpad-openssl iw iwinfo wireless-regdb kmod-cfg80211 kmod-mac80211 luci-app-temp-status"
+        EXCLUDED+=" -dnsmasq"
+    elif [ "${BASE}" == "immortalwrt" ]; then
+        PACKAGES+=" wpad-openssl iw iwinfo wireless-regdb kmod-cfg80211 kmod-mac80211 luci-app-temp-status"
+        EXCLUDED+=" -dnsmasq -cpusage -automount -libustream-openssl -default-settings-chn -luci-i18n-base-zh-cn"
+        if [ "$ARCH_2" == "x86_64" ]; then
+            EXCLUDED+=" -kmod-usb-net-rtl8152-vendor"
+        fi
+    fi
+}
+
+# Main build function
+build_firmware() {
+    local profile=$1
+    local tunnel_option=$2
+
+    log "INFO" "Starting build for profile: $profile"
+    
+    # Handle packages based on profile and tunnel option
+    handle_profile_packages "$profile"
+    handle_tunnel_option "$tunnel_option"
+    handle_release_packages
+    
+    # Custom Files
+    FILES="files"
+    
+    log "INFO" "Building image..."
+    make image PROFILE="$profile" PACKAGES="$PACKAGES $EXCLUDED" FILES="$FILES" 2>&1
+    
+    if [ ${PIPESTATUS[0]} -eq 0 ]; then
+        log "INFO" "Build completed successfully!"
+    else
+        log "ERROR" "Build failed. Check log for details."
+    fi
+}
+
+# Main script execution
+if [ -z "$1" ]; then
+    log "ERROR" "Profile not specified"
 fi
 
-# Custom Files
-FILES="files"
-
-# Disable service
-DISABLED_SERVICES="AdGuardHome"
-
-# Start build firmware
-make image PROFILE="$1" PACKAGES="$PACKAGES $EXCLUDED" FILES="$FILES" DISABLED_SERVICES="$DISABLED_SERVICES"
+build_firmware "$1" "$2"
